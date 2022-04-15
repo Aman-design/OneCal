ebc48112b1c959554b2c198ce6a0fc4186990079
Agusti Fernandez Pardo <git@agusti.me>
2022-04-15 21:34:02 +0200

    add redirect docs.cal.com/api -> developer.cal.com

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -3,4 +3,24 @@ const withNextra = require("nextra")({
   themeConfig: "./theme.config.js",
   unstable_staticImage: true,
 });
-module.exports = withNextra();
+module.exports = withNextra({
+  async rewrites() {
+    return [
+      // This redirects requests recieved at / the root to the /api/ folder.
+      {
+        source: "/api",
+        destination: "https://developer.cal.com/",
+        // permanent: false,
+      },
+      // {
+      //   source: "/api/:rest*",
+      //   destination: "https://developer.cal.com/:rest*",
+      // },
+      // This redirects requests to api/v*/ to /api/ passing version as a query parameter.
+      // {
+      //   source: "/api/v:version/:rest*",
+      //   destination: "/api/:rest*?version=:version",
+      // },
+    ];
+  },
+});
