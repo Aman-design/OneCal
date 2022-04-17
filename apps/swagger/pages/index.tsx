93f4afe26bd4607cc59915181ef3b93156192c25
Agusti Fernandez Pardo <git@agusti.me>
2022-04-17 18:57:05 +0200

    improve styling of swagger

--- apps/swagger/pages/index.tsx
+++ apps/swagger/pages/index.tsx
@@ -21,18 +21,19 @@ const requestSnippets = {
     },
   },
   defaultExpanded: true,
-  languages: ["curl_bash"],
-  // e.g. only show curl bash = ["curl_bash"]
+  languages: ["curl_bash", "curl_powershell", "curl_cmd", "node"],
+  // e.g. only shxwow curl bash = ["curl_bash"]
 };
 export default function APIDocs() {
   return (
     <SwaggerUI
       requestSnippets={requestSnippets}
       requestSnippetsEnabled={true}
-      docExpansion="none"
+      docExpansion="list"
       operationsSorter="method"
       filter={true}
-      url={process.env.NEXT_PUBLIC_SWAGGER_DOCS_URL || "https://api.cal.com/api/docs"}
+      tagsSorter="alpha"
+      url={process.env.NEXT_PUBLIC_SWAGGER_DOCS_URL || "http://localhost:3002/docs"}
     />
   );
 }
