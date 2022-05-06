44145ed4200d07e8968423ef4efee34375108af7
Agusti Fernandez Pardo <git@agusti.me>
2022-05-06 21:05:51 +0200

    fix: add sdk w tests

--- packages/sdk/README.md
+++ packages/sdk/README.md
@@ -1,13 +1,57 @@
-[![Codecov Coverage](https://img.shields.io/codecov/c/github/caki0915/my-awesome-greeter/coverage.svg?style=flat-square)](https://codecov.io/gh/caki0915/my-awesome-greeter/)
-
 # Cal.com Public API SDK
-This is an SDK for our API at https://api.cal.com
+
+Cal.com SDK for JavaScript
+Develop and deploy applications with the Cal.com SDK for JavaScript. The SDK provides first class TypeScript support and makes it easy to call Cal.com API resources using idiomatic JavaScript APIs to build Node.js, web, and mobile web applications on top of our -**app store for time**-
+
+## Stack
+
+### SDK Client auto-generated from our OpenAPI spec
+
+[Cal.com OpenAPI Spec](https://api.cal.com/docs)
 
 It uses `readmeio/api` package to auto-generate a javascript client with all the same available functions matching our API endpoints.
 
+We also extend it with our own types, so you can get autocompletion for all available methods on your IDE when using our SDK
 ## How to use
 
-`import calComSdk from 'calcom/sdk';`
+```
+import { cal } from '@calcom/sdk';
 
 describe('Cal.com SDK', () => {
-  calComSdk.auth('cal_{YOUR_API_KEY_HERE}');
+  cal.auth('cal_{YOUR_API_KEY_HERE}');
+
+  cal.listBookings();
+  // returns an array with all your bookings
+  
+```
+
+## Environment variables
+
+`CAL_API_URL=http://localhost:3002/v1/docs`
+If none provided, it defaults to our production API endpoint at: `https://api.cal.com`
+
+`CAL_API_KEY=cal_YOUR_API_KEY_GOES_HERE`
+CAL_API_KEY is used for automatically authenticating you when using the SDK, you can also choose to ignore using it and call cal.auth() yourself.
+
+## I am self-hosted (enterprise edition) licensed user, can I use this SDK with my API too?
+
+Yes, you can pass your own environment var, pointing to a valid openapi spec at <https://your-api-endpoint.com/docs>
+
+## How to develop
+
+- Install all the package dependencies with `yarn install`
+- yarn prepare will also make `yarn build` (tsc) run automatically after install.
+
+## Testing
+
+We use jest, and test all operationId's for our OpenAPI.
+`yarn test`
+
+## Formatting (prettier ) and Linting (ESLint)
+
+`yarn format`
+`yarn lint`
+
+## Updates to OpenAPI spect
+
+`yarn reset`
--- packages/sdk/src/__tests__/sdk.test.ts
+++ packages/sdk/src/__tests__/sdk.test.ts
@@ -1,148 +1,154 @@
-import sdk from '../index';
+import { cal } from "../index";
 
-describe('Cal.com SDK', () => {
-  sdk.auth(process.env.CAL_API_KEY);
-  describe('Users', () => {
+describe("Cal.com SDK", () => {
+  describe("Users", () => {
     // API Routes
-    it(`sdk.get('/users') should return a list of users`, async () => {
-      await sdk.get('/users').then((users: any) => {
+    it(`cal.get('/users') should return a list of users`, async () => {
+      await cal.get("/users").then((data: any) => {
+        expect(data.users).toBeTruthy();
+      });
+    });
+    it(`cal.get('/users/4') should return a user`, async () => {
+      await cal.get("/users/4").then((data: any) => {
+        expect(data.user).toBeTruthy();
+        expect(data.user.id).toBe(4);
+      });
+    });
+    it(`cal.delete('/users/1') should fail to remove a user that is not owner of apiKey`, async () => {
+      await cal.delete("/users/1").then((data: any) => {
+        expect(data.status).toBe(401);
+        expect(data.statusText).toBe("Unathorized");
+      });
+    });
+    // With Operation IDs
+    it("cal.listUsers() returns a list of users", async () => {
+      await cal.listUsers().then((users: any) => {
         expect(users).toBeTruthy();
       });
     });
-    it(`sdk.get('/users/4') should return a user`, async () => {
-      await sdk.get('/users/4').then((users: any) => {
-        expect(users).toBeTruthy();
-      });
-    });
-    // Operation IDs
-    it('sdk.listUsers() returns a list of users', async () => {
-      await sdk.listUsers().then((users: any) => {
-        expect(users).toBeTruthy();
-      });
-    });
-
-    it('sdk.getUserById() returns a user', async () => {
-      await sdk
+    let user;
+    it("cal.getUserById() returns a user", async () => {
+      user = await cal
         .getUserById({
-          id: 4,
+          id: 4
         })
         .then((data: any) => {
           expect(data.user).toBeTruthy();
         });
     });
-    it('sdk.editUserById() edits a user', async () => {
-      await sdk
+    it("cal.editUserById() edits a user", async () => {
+      await cal
         .editUserById(
           {
-            timeZone: 'Europe/Madrid',
+            timeZone: "Europe/Madrid"
           },
           {
-            id: 4,
-          },
+            id: 4
+          }
         )
         .then((data: any) => {
           expect(data.user).toBeTruthy();
-          expect(data.user.timeZone).toBe('Europe/Madrid');
+          expect(data.user.timeZone).toBe("Europe/Madrid");
         });
     });
-    let newUser: any;
-    newUser &&
-      it('sdk.removeUserById() should remove an user', async () => {
-        await sdk
-          .removeUserById({
-            id: newUser.data.id,
-          })
-          .then((data: any) => {
-            expect(data.message).toBeTruthy();
-          });
-      });
+    // it('cal.removeUserById() should remove an user', async () => {
+    //   await cal
+    //     .removeUserById({
+    //       id: 4,
+    //     })
+    //     .then((data: any) => {
+    //       expect(data.message).toBeTruthy();
+    //     });
+    // });
   });
 
-  describe('Attendees', () => {
-    // Attendees
-    it('should return a list of attendees', async () => {
-      await sdk.listAttendees().then((data: any) => {
+  // Attendees
+  describe("Attendees", () => {
+    it("should return a list of attendees", async () => {
+      await cal.listAttendees().then((data: any) => {
         expect(data.attendees).toBeTruthy();
       });
     });
-    it('should return an attendee by ID', async () => {
-      await sdk
-        .getAttendeeById({
-          id: 25,
-        })
-        .then((data: any) => {
-          expect(data.attendee).toBeTruthy();
-          expect(data.attendee.timeZone).toBe('Europe/Madrid');
-        });
-    });
-    it('should edit an attendee', async () => {
-      await sdk
-        .editAttendeeById(
-          {
-            timeZone: 'Europe/Madrid',
-          },
-          {
-            id: 25,
-          },
-        )
-        .then((data: any) => {
-          expect(data.attendee).toBeTruthy();
-          expect(data.attendee.timeZone).toBe('Europe/Madrid');
-        });
-    });
     let newAttendee: any;
-    it('should create a new attendee', async () => {
-      await sdk
+
+    it("should create a new attendee", async () => {
+      await cal
         .addAttendee({
           bookingId: 1,
-          name: 'John Doe',
-          email: 'john@doe.com',
-          timeZone: 'Europe/Madrid',
+          name: "John Doe",
+          email: "john@doe.com",
+          timeZone: "Europe/Madrid"
         })
         .then((data: any) => {
           newAttendee = data.attendee;
           expect(data.attendee).toBeTruthy();
-          expect(data.attendee.timeZone).toBe('Europe/Madrid');
+          expect(data.attendee.timeZone).toBe("Europe/Madrid");
+        });
+    });
+    it("should return an attendee by ID", async () => {
+      await cal
+        .getAttendeeById({
+          id: 1
+        })
+        .then((data: any) => {
+          expect(data.attendee).toBeTruthy();
+          expect(data.attendee.timeZone).toBe("Europe/Madrid");
+        });
+    });
+    it("should edit an attendee", async () => {
+      await cal
+        .editAttendeeById(
+          {
+            timeZone: "Europe/Madrid"
+          },
+          {
+            id: 1
+          }
+        )
+        .then((data: any) => {
+          expect(data.attendee).toBeTruthy();
+          expect(data.attendee.timeZone).toBe("Europe/Madrid");
         });
     });
     newAttendee &&
-      it('should remove an attendee', async () => {
-        await sdk
+      it("should remove an attendee", async () => {
+        await cal
           .removeAttendeeById({
-            id: newAttendee.data.id,
+            id: newAttendee.data.id
           })
           .then((data: any) => {
             expect(data.message).toBeTruthy();
           });
       });
   });
-  describe('Availabilities', () => {
-    // Availabilities
-    it('should return a list of availabilities', async () => {
-      await sdk.listAvailabilities().then((data: any) => {
+
+  // Availabilities
+  describe("Availabilities", () => {
+    it("should return a list of availabilities", async () => {
+      await cal.listAvailabilities().then((data: any) => {
         expect(data.availabilities).toBeTruthy();
       });
     });
-    it('should return an availability by ID', async () => {
-      await sdk
+    it("should return an availability by ID", async () => {
+      await cal
         .getAvailabilityById({
-          id: 1,
+          id: 14
         })
         .then((data: any) => {
           expect(data.availability).toBeTruthy();
           expect(data.availability.days).toHaveLength(4);
         });
     });
-    it('should edit an availability', async () => {
-      await sdk
+    it("should edit an availability", async () => {
+      await cal
         .editAvailabilityById(
           {
             endTime: new Date(),
-            days: [1, 2, 3, 4],
+            days: [1, 2, 3, 4]
           },
           {
-            id: 1,
-          },
+            id: 14
+          }
         )
         .then((data: any) => {
           expect(data.availability).toBeTruthy();
@@ -150,12 +156,12 @@ describe('Cal.com SDK', () => {
         });
     });
     let newAvailability: any;
-    it('should create a new availability', async () => {
-      await sdk
+    it("should create a new availability", async () => {
+      await cal
         .addAvailability({
           days: [1, 2, 3, 4, 5],
-          startTime: '1970-01-01T09:00:00.000Z',
-          endTime: '1970-01-01T17:00:00.000Z',
+          startTime: new Date(),
+          endTime: new Date()
         })
         .then((data: any) => {
           newAvailability = data.availability;
@@ -164,10 +170,137 @@ describe('Cal.com SDK', () => {
         });
     });
     newAvailability &&
-      it('should remove an availability', async () => {
-        await sdk
+      it("should remove an availability", async () => {
+        await cal
           .removeAvailabilityById({
-            id: newAvailability.data.id,
+            id: newAvailability.data.id
+          })
+          .then((data: any) => {
+            expect(data.message).toBeTruthy();
+          });
+      });
+  });
+
+  // BookingReferences
+  describe("BookingReferences", () => {
+    it("should return a list of booking_references", async () => {
+      await cal.listBookingReferences().then((data: any) => {
+        expect(data.booking_references).toBeTruthy();
+      });
+    });
+    it("should return an booking_reference by ID", async () => {
+      await cal
+        .getBookingReferenceById({
+          id: 1
+        })
+        .then((data: any) => {
+          expect(data.booking_reference).toBeTruthy();
+          expect(data.booking_reference.uid).toBe("12345");
+        });
+    });
+    it("should edit an booking_reference", async () => {
+      await cal
+        .editBookingReferenceById(
+          {
+            type: "new-type",
+            meetingId: "another-meeting"
+          },
+          {
+            id: 1
+          }
+        )
+        .then((data: any) => {
+          expect(data.booking_reference).toBeTruthy();
+          expect(data.booking_reference.type).toBe("new-type");
+          expect(data.booking_reference.meetingId).toBe("another-meeting");
+        });
+    });
+    let newBookingReference: any;
+    it("should create a new booking reference", async () => {
+      await cal
+        .addBookingReference({
+          uid: "12345",
+          type: "type",
+          meetingId: "ameeting",
+          deleted: false,
+          bookingId: 1
+        })
+        .then((data: any) => {
+          newBookingReference = data.booking_reference;
+          expect(data.booking_reference).toBeTruthy();
+          expect(data.booking_reference.uid).toBe("12345");
+        });
+    });
+    newBookingReference &&
+      it("should remove an booking reference", async () => {
+        await cal
+          .removeBookingReferenceById({
+            id: newBookingReference.data.id
+          })
+          .then((data: any) => {
+            expect(data.message).toBeTruthy();
+          });
+      });
+  });
+
+  // Bookings
+  describe("Bookings", () => {
+    it("should return a list of bookings", async () => {
+      await cal.listBookings().then((data: any) => {
+        expect(data.bookings).toBeTruthy();
+      });
+    });
+    it("should return an booking by ID", async () => {
+      await cal
+        .getBookingById({
+          id: 1
+        })
+        .then((data: any) => {
+          expect(data.booking).toBeTruthy();
+          console.log(data.booking);
+          expect(data.booking.uid).toBe("b0100b35-e0d9-415e-95e6-eaa3de41a963");
+        });
+    });
+    it("should edit an booking", async () => {
+      await cal
+        .editBookingById(
+          {
+            uid: "12345"
+
+            // type: "new-type",
+            // meetingId: "another-meeting"
+          },
+          {
+            id: 1
+          }
+        )
+        .then((data: any) => {
+          expect(data.booking).toBeTruthy();
+          expect(data.booking.uid).toBe("12345");
+          // expect(data.booking.meetingId).toBe("another-meeting");
+        });
+    });
+    let newBooking: any;
+    it("should create a new booking", async () => {
+      await cal
+        .addBooking({
+          uid: "12345"
+          // type: "type",
+          // meetingId: "ameeting",
+          // deleted: false,
+          // bookingId: 1
+        })
+        .then((data: any) => {
+          newBooking = data.booking;
+          expect(data.booking).toBeTruthy();
+          expect(data.booking.uid).toBe("12345");
+        });
+    });
+    newBooking &&
+      it("should remove an booking", async () => {
+        await cal
+          .removeBookingById({
+            id: newBooking.data.id
           })
           .then((data: any) => {
             expect(data.message).toBeTruthy();
