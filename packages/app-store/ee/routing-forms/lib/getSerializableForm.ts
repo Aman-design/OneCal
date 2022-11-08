0a389bb292358d2219ebe4e31aba038bea7e56b3
Hariom Balhara <hariombalhara@gmail.com>
2022-11-08 19:51:53 +0530

    Support header in Routing Form CSV (#5133)
    * Dont actually delete fields and now add headers to CSV which is possible
    
    * Fix TS issues
    
    * Handle cases due to soft deletion
    
    * Add submission time as a column
    
    * Fix bug with fields getting deleted
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>
    Co-authored-by: Peer Richelsen <peeroke@gmail.com>

--- packages/app-store/ee/routing-forms/lib/getSerializableForm.ts
+++ packages/app-store/ee/routing-forms/lib/getSerializableForm.ts
@@ -3,7 +3,13 @@ import { App_RoutingForms_Form } from "@prisma/client";
 import { SerializableForm } from "../types/types";
 import { zodFields, zodRoutes } from "../zod";
 
-export function getSerializableForm<TForm extends App_RoutingForms_Form>(form: TForm) {
+/**
+ * Doesn't have deleted fields by default
+ */
+export function getSerializableForm<TForm extends App_RoutingForms_Form>(
+  form: TForm,
+  withDeletedFields = false
+) {
   const routesParsed = zodRoutes.safeParse(form.routes);
   if (!routesParsed.success) {
     throw new Error("Error parsing routes");
@@ -17,7 +23,12 @@ export function getSerializableForm<TForm extends App_RoutingForms_Form>(form: T
   // Ideally we shouldb't have needed to explicitly type it but due to some reason it's not working reliably with VSCode TypeCheck
   const serializableForm: SerializableForm<TForm> = {
     ...form,
-    fields: fieldsParsed.data,
+    settings: settings,
+    fields: fieldsParsed.data
+      ? withDeletedFields
+        ? fieldsParsed.data
+        : fieldsParsed.data.filter((f) => !f.deleted)
+      : [],
     routes: routesParsed.data,
     createdAt: form.createdAt.toString(),
     updatedAt: form.updatedAt.toString(),
