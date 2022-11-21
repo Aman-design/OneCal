e83c975809670f79e09a3aa4805ab6ab600a6f05
CarinaWolli <wollencarina@gmail.com>
2022-11-21 14:43:37 +0100

    allow to cancel and reschedule booking for unconfirmed bookings

--- apps/web/pages/success.tsx
+++ apps/web/pages/success.tsx
@@ -463,7 +463,7 @@ export default function Success(props: SuccessProps) {
                     )}
                   </div>
                 </div>
-                {!needsConfirmation &&
+                {(!needsConfirmation || !userIsOwner) &&
                   !isCancelled &&
                   (!isCancellationMode ? (
                     <>
