491bcbcee60b7c3cab2a2da307df4e5c32b924e9
sean-brydon <55134778+sean-brydon@users.noreply.github.com>
2023-03-05 17:09:43 +0800

    Phone number radius fix

--- packages/ui/form/PhoneInput.tsx
+++ packages/ui/form/PhoneInput.tsx
@@ -4,6 +4,8 @@ import BasePhoneInput from "react-phone-number-input";
 import type { Props, Country } from "react-phone-number-input";
 import "react-phone-number-input/style.css";
 
+import cn from "@calcom/lib/classNames";
+
 export type PhoneInputProps = Props<{
   value: string;
   id?: string;
@@ -25,9 +27,13 @@ function PhoneInput({ name, className = "", onChange, ...rest }: PhoneInputProps
       onChange={onChange}
       countrySelectProps={{ className: "text-black" }}
       numberInputProps={{
-        className: "border-0 text-sm focus:ring-0 dark:bg-darkgray-100 dark:placeholder:text-darkgray-600",
+        className:
+          "border-0 text-sm focus:ring-0 dark:bg-darkgray-100 dark:placeholder:text-darkgray-600 rounded-lg",
       }}
-      className={`${className} focus-within:border-brand dark:bg-darkgray-100 dark:border-darkgray-300 block w-full rounded-md rounded-sm border border border-gray-300 py-px pl-3 ring-black focus-within:ring-1 disabled:text-gray-500 disabled:opacity-50 dark:text-white dark:selection:bg-green-500 disabled:dark:text-gray-500`}
+      className={cn(
+        "focus-within:border-brand dark:bg-darkgray-100 dark:border-darkgray-300 block w-full rounded-md border border border-gray-300 py-px pl-3 ring-black focus-within:ring-1 disabled:text-gray-500 disabled:opacity-50 dark:text-white dark:selection:bg-green-500 disabled:dark:text-gray-500",
+        className
+      )}
     />
   );
 }
